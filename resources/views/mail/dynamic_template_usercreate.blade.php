305d89ae65c2ff356913639b319c9065bba8fab4
Tibo <tibodemunck@live.be>
2021-11-22 23:10:34 +0100

    deftige a href

--- resources/views/mail/dynamic_template_usercreate.blade.php
+++ resources/views/mail/dynamic_template_usercreate.blade.php
@@ -1,2 +1,2 @@
 <p>Welcome {{ $data->name }},</p>
-<p>Login to <a>https://osd.thewhizkids.be</a> with your email and password: {{ $data->password }} and reset your password.</p>
+<p>Login to <a href="https://osd.thewhizkids.be">OSD website</a> with your email and password: {{ $data->password }} and reset your password.</p>
